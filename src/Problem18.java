b4333e6dfd537b679ce70eae93c766e1cc87ce0c
Christian Gati <christian.gati@gmail.com>
2015-01-25 16:58:37 -0500

    Remove .idea files from repo

---
+++ src/Problem18.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Chris on 1/25/2015.
+ */
+public class Problem18 {
+    public static void main(String[] args) {
+        Problem18 problem = new Problem18();
+    }
+
+    public Problem18() {
+
+    }
+}
